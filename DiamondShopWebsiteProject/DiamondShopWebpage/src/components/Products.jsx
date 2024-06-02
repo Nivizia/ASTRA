981cc282e924fb8976d7d90b7de95d173fb7e9a4
MrNoFunsadface <152047068+MrNoFunsadface@users.noreply.github.com>
2024-06-03 00:46:57 +0700

    Readded input.css, output.css

--- DiamondShopWebsiteProject/DiamondShopWebpage/src/components/Products.jsx
+++ DiamondShopWebsiteProject/DiamondShopWebpage/src/components/Products.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import {Textarea} from "@nextui-org/react";
 
 export default function Products() {
   return (
-    <div>Products</div>
-  )
+    <Textarea
+      label="Description"
+      placeholder="Enter your description"
+      className="max-w-xs"
+    />
+  );
 }
