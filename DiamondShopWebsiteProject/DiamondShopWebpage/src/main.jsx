8a22a35a659d9263bb8bcedb3abd6b9c5e5ab4ab
MrNoFunsadface <152047068+MrNoFunsadface@users.noreply.github.com>
2024-06-12 13:09:39 +0700

    Remade example page
    wah

--- DiamondShopWebsiteProject/DiamondShopWebpage/src/main.jsx
+++ DiamondShopWebsiteProject/DiamondShopWebpage/src/main.jsx
@@ -1,13 +1,58 @@
+//Import
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+//Component Imports
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
+import EditContract, {
+  action as editAction,
+}
+from './routes/edit';
+
+
+//Css Imports
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        errorElement: <ErrorPage />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContract />,
+        errorElement: <ErrorPage />,
+        loader: contactLoader,
+        action: editAction,
+      }
+    ],
+  },
+  
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
