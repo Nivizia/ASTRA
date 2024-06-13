eb1f96e32d4c2dfffd544033b7216bc94be61efb
MrNoFunsadface <152047068+MrNoFunsadface@users.noreply.github.com>
2024-06-13 10:12:05 +0700

    ok

--- DiamondShopWebsiteProject/DiamondShopWebApp/src/main.jsx
+++ DiamondShopWebsiteProject/DiamondShopWebApp/src/main.jsx
@@ -1,10 +1,26 @@
+//React Imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+//Component Imports
+import Root from './routes/root';
+
+//Css Imports
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ DiamondShopWebsiteProject/DiamondShopWebApp/src/routes/root.jsx
@@ -0,0 +1,8 @@
+const Root = () => {
+  return (
+    <a href="https://ibb.co/WkG0VQP">
+        <img src="https://i.ibb.co/1r7Mz3d/Blush.jpg" alt="Blush" border="0"/>
+    </a>
+  )
+}
+export default Root
