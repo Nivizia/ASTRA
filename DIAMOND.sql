0db79373610f7616ec0934df32e3faa09796d77f
yogurtvn <lethanhdatct.2003@gmail.com>
2024-06-20 14:10:21 +0700

    Update DIAMOND.sql

--- DIAMOND.sql
+++ DIAMOND.sql
@@ -6,6 +6,6 @@ VALUES
 ('Brilliant Cut Diamond', 5000.00, 'Round', '1.0', 'D', 'VVS1', 'Excellent'),
 ('Princess Cut Diamond', 4000.00, 'Square', '1.2', 'E', 'VS1', 'Very Good'),
 ('Oval Cut Diamond', 4500.00, 'Oval', '1.5', 'F', 'SI1', 'Good'),
-('Marquise Cut Diamond', 'Marquise', '1.8', 'G', 'VVS2', 'Excellent'),
+('Marquise Cut Diamond', 4800.00, 'Marquise', '1.8', 'G', 'VVS2', 'Excellent'),
 ('Emerald Cut Diamond', 4700.00, 'Rectangular', '2.0', 'H', 'VS2', 'Very Good');
 GO
