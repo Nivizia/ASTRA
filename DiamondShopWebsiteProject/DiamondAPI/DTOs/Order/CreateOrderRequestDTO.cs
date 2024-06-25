d124b17321204e8e468b2fec224a6e222daa12f0
Nguyễn Khang Bình <binhnkse182311@fpt.edu.vn>
2024-06-25 16:21:03 +0700

    update cart endpoint

---
+++ DiamondShopWebsiteProject/DiamondAPI/DTOs/Order/CreateOrderRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using DiamondAPI.DTOs.Orderitem;
+
+namespace DiamondAPI.DTOs.Order
+{
+    public class CreateOrderRequestDTO
+    {
+        public Guid? CustomerId { get; set; }
+
+        public decimal? TotalAmount { get; set; }
+
+        public string? OrderStatus { get; set; }
+
+        public virtual List<CreateOrderitemRequestDTO> Orderitems { get; set; } = new List<CreateOrderitemRequestDTO>();
+    }
+}
