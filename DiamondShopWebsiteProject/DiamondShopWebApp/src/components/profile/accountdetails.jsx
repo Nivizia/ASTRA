2472cbccf509432f220756c64e5cc88566eced8a
Nguyễn Khang Bình <binhnkse182311@fpt.edu.vn>
2024-07-07 01:16:34 +0700

    working on profile

--- DiamondShopWebsiteProject/DiamondShopWebApp/src/components/profile/accountdetails.jsx
+++ DiamondShopWebsiteProject/DiamondShopWebApp/src/components/profile/accountdetails.jsx
@@ -1,21 +1,45 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
+import styles from '../css/accountdetails.module.css';
 
-const accountdetails = () => {
-    const { user, logout } = useContext(AuthContext);
-    const [FirstName, setFirstName] = useState('');
-
-    const handleclick = () => {
-        console.log(user);
-    }
+const AccountDetails = () => {
+    const { user } = useContext(AuthContext);
 
     return (
-        <div>
+        <div className={styles.container}>
             <h2>Account Details</h2>
             <p>View and manage your personal details and contact information</p>
-            <button onClick={handleclick}>user</button>
+            <div className={styles.detailsSection}>
+                <div className={styles.detailsColumn}>
+                    <h4 className={styles.detailsTitle}>Login Details:</h4>
+                </div>
+                <div className={styles.detailsColumn}>
+                    <div className={styles.detailsRow}>
+                        <div className={styles.detailsLabel}>First Name:</div>
+                        <div className={styles.detailsValue}>{user.FirstName || '-'}</div>
+                    </div>
+                    <div className={styles.detailsRow}>
+                        <div className={styles.detailsLabel}>Last Name:</div>
+                        <div className={styles.detailsValue}>{user.LastName || '-'}</div>
+                    </div>
+                    <div className={styles.detailsRow}>
+                        <div className={styles.detailsLabel}>Phone:</div>
+                        <div className={styles.detailsValue}>{user.phone || '-'}</div>
+                    </div>
+                </div>
+                <div className={styles.detailsColumn}>
+                    <div className={styles.detailsRow}>
+                        <div className={styles.detailsLabel}>Email:</div>
+                        <div className={styles.detailsValue}>{user.Email || '-'}</div>
+                    </div>
+                    <div className={styles.detailsRow}>
+                        <div className={styles.detailsLabel}>Current Password:</div>
+                        <div className={styles.detailsValue}>**********</div>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default accountdetails
+export default AccountDetails;
