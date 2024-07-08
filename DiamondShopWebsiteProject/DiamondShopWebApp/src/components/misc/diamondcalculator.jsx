42a2dfd6c9ee6c5aab64267af42ea9325ace784d
MrNoFunsadface <152047068+MrNoFunsadface@users.noreply.github.com>
2024-07-08 17:34:22 +0700

    diamond calculator page update

--- DiamondShopWebsiteProject/DiamondShopWebApp/src/components/misc/diamondcalculator.jsx
+++ DiamondShopWebsiteProject/DiamondShopWebApp/src/components/misc/diamondcalculator.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import { calculateDiamondPrice, calculateDiamondPricePerCarat } from '../../../javascript/apiService';
+
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import { Tooltip } from '@mui/material';
+
 import styles from "../css/diamondcalculator.module.css";
 
 const DiamondPriceCalculator = () => {
@@ -31,53 +36,73 @@ const DiamondPriceCalculator = () => {
         }
     };
 
+    function valuetext(value) {
+        return `${value}°C`;
+    }
+
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Diamond Price Calculator</h1>
-            <input
-                type="number"
-                placeholder="Carat"
-                value={carat}
-                onChange={e => setCarat(e.target.value)}
-                className={styles.inputField}
-            />
-            <input
-                type="text"
-                placeholder="Color"
-                value={color}
-                onChange={e => setColor(e.target.value)}
-                className={styles.inputField}
-            />
-            <input
-                type="text"
-                placeholder="Clarity"
-                value={clarity}
-                onChange={e => setClarity(e.target.value)}
-                className={styles.inputField}
-            />
-            <input
-                type="text"
-                placeholder="Cut"
-                value={cut}
-                onChange={e => setCut(e.target.value)}
-                className={styles.inputField}
-            />
-            <button
-                onClick={handleCalculatePrice}
-                className={`${styles.button} ${styles.calculateButton}`}
-            >
-                Calculate Price
-            </button>
-            <button
-                onClick={handleCalculatePricePerCarat}
-                className={`${styles.button} ${styles.calculatePerCaratButton}`}
-            >
-                Calculate Price Per Carat
-            </button>
-            {price !== null && <p className={styles.result}>Total Price: ${price.toFixed(2)}</p>}
-            {pricePerCarat !== null && <p className={styles.result}>Price Per Carat: ${pricePerCarat.toFixed(2)}</p>}
-            {error && <p className={styles.error}>Error: {error}</p>}
-        </div>
+        <>
+
+            <div className={styles.container}>
+                <h1 className={styles.title}>Diamond Price Calculator</h1>
+                <Box className={styles.inputField}>
+                    <Slider
+                        aria-label="Carat"
+                        defaultValue={1}
+                        getAriaValueText={valuetext}
+                        valueLabelDisplay='auto'
+                        shiftStep={30}
+                        step={0.01}
+                        min={0.1}
+                        max={5}
+                    />
+                </Box>
+                <input
+                    type="number"
+                    placeholder="Carat"
+                    value={carat}
+                    onChange={e => setCarat(e.target.value)}
+                    className={styles.inputField}
+                />
+                <input
+                    type="text"
+                    placeholder="Color"
+                    value={color}
+                    onChange={e => setColor(e.target.value)}
+                    className={styles.inputField}
+                />
+                <input
+                    type="text"
+                    placeholder="Clarity"
+                    value={clarity}
+                    onChange={e => setClarity(e.target.value)}
+                    className={styles.inputField}
+                />
+                <input
+                    type="text"
+                    placeholder="Cut"
+                    value={cut}
+                    onChange={e => setCut(e.target.value)}
+                    className={styles.inputField}
+                />
+                <button
+                    onClick={handleCalculatePrice}
+                    className={`${styles.button} ${styles.calculateButton}`}
+                >
+                    Calculate Price
+                </button>
+                <button
+                    onClick={handleCalculatePricePerCarat}
+                    className={`${styles.button} ${styles.calculatePerCaratButton}`}
+                >
+                    Calculate Price Per Carat
+                </button>
+                {price !== null && <p className={styles.result}>Total Price: ${price.toFixed(2)}</p>}
+                {pricePerCarat !== null && <p className={styles.result}>Price Per Carat: ${pricePerCarat.toFixed(2)}</p>}
+                {error && <p className={styles.error}>Error: {error}</p>}
+            </div>
+
+        </>
     );
 };
 
